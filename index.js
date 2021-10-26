1abc3af5c1801806cfc95c62c036412b89a21347
Amelia Hooper <ajhooper97@gmail.com>
2021-10-26 13:09:20 +1100

    index.js functions questions

--- index.js
+++ index.js
@@ -5,100 +5,135 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 const fs = require('fs');
+const { type } = require('os');
 
 const teamProfile = [];
 
-const managerPrompt = () => {
-    return new Promise((res, rej) => {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: `What is the manager's name?`,
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: `Please provide the manager's ID`,
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: `Please provide the manager's email address`,
-            },
-            {
-                type: 'input',
-                name: 'officeNumber',
-                message: `Please provide manager's office number`,
-            },
-        ]).then(answers => {
-            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-            teamProfile.push(manager);
-            res();
-        });
+const firstQuestionPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'members',
+            message: `Do you want to add a team member?`,
+            choices: ['Yes', 'No'],
+        }
+    ]).then(answers => {
+        const addTeamMember = answers.members;
+        return addTeamMember === 'Yes'
+    })
+}
+
+async function startQuestions() {
+    let addTeamMember = await firstQuestionPrompt();
+    while (addTeamMember) {
+        await employeeTypePrompt();
+        addTeamMember = await firstQuestionPrompt();
+    }
+    createTeamProfile(); // function that puts info into HTML page
+}
+
+const employeeTypePrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'typeOfEmployee',
+            message: `What type of employee would you like to add?`,
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+    ]).then(answers => {
+        const employeeType = answers.typeOfEmployee;
+        if (employeeType === 'Manager') {
+            return managerPrompt();
+        } else if (employeeType === 'Engineer') {
+            return engineerPrompt();
+        } else if (employeeType === 'Intern') {
+            return internPrompt();
+        }
     });
 }
 
-managerPrompt();
+const managerPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: `What is the manager's name?`,
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `Please provide the manager's ID`,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `Please provide the manager's email address`,
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: `Please provide manager's office number`,
+        },
+    ]).then(answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        teamProfile.push(manager);
+    });
+}
 
 const engineerPrompt = () => {
-    return new Promise((res, rej) => {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: `What is the engineer's name?`,
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: `Please provide the engineer's ID`,
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: `Please provide the engineer's email address`,
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: `Please provide engineer's GitHub username`,
-            },
-        ]).then(answers => {
-            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-            teamProfile.push(engineer);
-            res();
-        });
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: `What is the engineer's name?`,
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `Please provide the engineer's ID`,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `Please provide the engineer's email address`,
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: `Please provide engineer's GitHub username`,
+        },
+    ]).then(answers => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        teamProfile.push(engineer);
     });
 }
 
 const internPrompt = () => {
-    return new Promise((res, rej) => {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is the intern name?',
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: 'Please provide the intern ID',
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'Please provide the intern email address',
-            },
-            {
-                type: 'input',
-                name: 'school',
-                message: 'Please provide the intern school name',
-            }
-        ]).then(answers => {
-            const intern = new Intern(answers.name, anwers.id, answers.email, answers.school);
-            teamProfile.push(intern);
-            res();
-        });
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the intern name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please provide the intern ID',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please provide the intern email address',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Please provide the intern school name',
+        }
+    ]).then(answers => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        teamProfile.push(intern);
     });
-} 
+}
+
+startQuestions();
